77c818c6fd2cc113c85e57cea300fe23c627ce49
Julio Olivera <julio.olvr@gmail.com>
2016-06-02 23:20:43 -0300

    Webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: "/dist",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /.js$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  })]
+};
