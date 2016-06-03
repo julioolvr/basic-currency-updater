624c5a3fd1c55fd180695e946b1f1c5f877051f0
Julio Olivera <julio.olvr@gmail.com>
2016-06-02 23:39:12 -0300

    Moved HTML to App component

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,58 @@ import React from 'react'
 
 export const App = () => {
   return (
-    <h1>Hello, world!</h1>
+    <div>
+      <div>
+        From:
+        <ul>
+          <li>
+            <div>
+              <select defaultValue="USD">
+                <option value="USD">USD</option>
+                <option value="ARS">ARS</option>
+                <option value="GBP">GBP</option>
+                <option value="EUR">EUR</option>
+              </select>
+              <input type="number" value="0.96"></input>
+              <input type="number" value="25"></input>
+            </div>
+          </li>
+          <li>
+            <div>
+              <select defaultValue="GBP">
+                <option value="USD">USD</option>
+                <option value="ARS">ARS</option>
+                <option value="GBP">GBP</option>
+                <option value="EUR">EUR</option>
+              </select>
+              <input type="number" value="0.8"></input>
+              <input type="number" value="30"></input>
+            </div>
+          </li>
+          <li>
+            <div>
+              <select defaultValue="EUR">
+                <option value="USD">USD</option>
+                <option value="ARS">ARS</option>
+                <option value="GBP">GBP</option>
+                <option value="EUR">EUR</option>
+              </select>
+              <input type="number" value="0.7"></input>
+              <input type="number" value="50"></input>
+            </div>
+          </li>
+        </ul>
+        <div>
+          To:
+          <select defaultValue="ARS">
+            <option value="USD">USD</option>
+            <option value="ARS">ARS</option>
+            <option value="GBP">GBP</option>
+            <option value="EUR">EUR</option>
+          </select>, ARS1234.45
+        </div>
+      </div>
+    </div>
   )
 }
 
